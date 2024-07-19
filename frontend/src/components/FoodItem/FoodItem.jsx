9379f91c1785ab55e2d4b25f3432d2a20c748284
sjeuquay <125850446+sjeuquay@users.noreply.github.com>
2024-07-19 10:27:09 +0700

    header navbar menu

---
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,23 @@
+// import React from 'react'
+import './FoodItem.css'
+import { assets } from '../../assets/assets'
+
+const FoodItem = ({id, name, price, description, image}) => {
+  return (
+    <div className='food-item'>
+        <div className="food-item-img-container">
+            <img className='food-item-image' src={image} alt="" />
+        </div>
+        <div className="food-item-info">
+            <p>{name}</p>
+            <img src={assets.rating_starts} alt="" />
+        </div>
+        <p className="food-item-description">
+            {description}
+        </p>
+        <p className="food-item-price">${price}</p>
+    </div>
+  )
+}
+
+export default FoodItem
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-content">
+            <h2>Order your favourite food here</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore in et sed ipsam voluptatem repudiandae necessitatibus, doloremque omnis dicta dignissimos itaque praesentium aperiam vel deserunt ea, tenetur nisi cum nobis!</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
